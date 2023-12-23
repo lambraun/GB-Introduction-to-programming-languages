4106404e665ab2b349617295aad8c8eec3a97e90
unknown <lambraun@rambler.ru>
2023-12-23 20:39:08 +0300

    Added a second task

--- Program.cs
+++ Program.cs
@@ -14,17 +14,31 @@
 // int input = Convert.ToInt32(Console.ReadLine());
 // Console.WriteLine(input + 1000);
 
-Console.Write("Введите первое число: ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber == secondNumber * secondNumber)
+// if(firstNumber == secondNumber * secondNumber)
+// {
+//     Console.Write("Да, " + firstNumber + " является кв. от числа " + secondNumber);
+// }
+// else
+// {
+//     Console.Write("Нет, " + firstNumber + " не является кв. от числа " + secondNumber);
+// }
+
+// Напишите программу, которая на вход принимает
+// целое число N, а на выходе показывает все целые
+// числа в промежутке от -N до N.
+
+Console.Write("Введите число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int negativeN = N * -1;
+
+while(negativeN <= N)
 {
-    Console.Write("Да, " + firstNumber + " является кв. от числа " + secondNumber);
-}
-else
-{
-    Console.Write("Нет, " + firstNumber + " не является кв. от числа " + secondNumber);
+    Console.Write(negativeN + ", ");
+    negativeN++;               //(negativeN = negativeN + 1)
 }
